65bdd39976ffe4d1f168076d0ca04e2be20f71c9
RogierdeRuijter <rogierderuijter@hotmail.com>
2023-03-15 17:56:48 +0100

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
   site: 'https://rogierderuijter.github.io',
   base: '/blog',
   integrations: [mdx(), sitemap()],
